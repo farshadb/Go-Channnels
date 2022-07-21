d602d822e40ad95f7ab1784bf72f6e741f3a4578
F4r5h4d <bolouri.farshad@gmail.com>
2022-07-21 17:09:25 +0430

    first channel created and passed as a parammetet to another goroutin.

---
+++ firsChannel/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func Print(c chan bool) {
+	fmt.Println("Printing form goroutine")
+	c <- true
+}
+
+func main() {
+
+	c :=make(chan bool)
+	// this line will print c addres in the memory
+	fmt.Println(c)
+	// Below line will return fatal error  because deadlock - no gorutine has been started.
+	//a := <-c
+	//fmt.Println(a)
+	// Sending channel c as parameter to Print Goroutine 
+
+	go Print(c)
+	b := <- c
+    fmt.Println("Printing form main(main also is a goroutine).")
+	fmt.Println("Recieved: ", b)
+
+	
+}
